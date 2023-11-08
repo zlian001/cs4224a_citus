0848e92f2cc1c57febeeced75dc6754dfdbce79e
zlian001 <e0949137@u.nus.edu>
2023-11-09 05:10:32 +0800

    txns defined

--- load-citus-data.sh
+++ load-citus-data.sh
@@ -9,21 +9,25 @@ logtime() {
     date +"%Y-%m-%dT%H:%M:%S:%3N -- "
 }
 
-${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -f ${SCRIPTSDIR}/schema.sql
+# ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -f ${SCRIPTSDIR}/schema.sql
 echo $(logtime) "created table schemas"
-echo $(logtime) "node ${NODE}: converting null strings to empty fields in data files" 
-# convert 'null' string data to -1 int value so cql query can query empty O_CARRIER_ID value
-sed 's/,null,/,-1,/' ${DATADIR}/order.csv > ${DATADIR}/order_null.csv
-# convert 'null' strings to empty field `,,`
-sed 's/,null,/,,/' ${DATADIR}/order-line.csv > ${DATADIR}/order-line_null.csv
 if [ ${NODE} = "$COORD" ]; then
-    ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('warehouse', 'w_id');"
-    ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('district', 'd_w_id');"
-    ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('customer', 'c_w_id');"
-    ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('customer_order', 'o_w_id');"
-    ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_reference_table('item');"
-    ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('order_line', 'ol_w_id');"
-    ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('stock', 's_w_id');"
+    echo $(logtime) "node ${NODE}: converting null strings to empty fields in data files" 
+    # convert 'null' string data to -1 int value so cql query can query empty O_CARRIER_ID value
+    sed 's/,null,/,-1,/' ${DATADIR}/order.csv > ${DATADIR}/order_null.csv
+    # convert 'null' strings to empty field `,,`
+    sed 's/,null,/,,/' ${DATADIR}/order-line.csv > ${DATADIR}/order-line_null.csv
+
+    # ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('warehouse', 'w_id');"
+    # ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('district', 'd_w_id');"
+    # ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('customer', 'c_w_id');"
+    # ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('customer_order', 'o_w_id');"
+    # ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_reference_table('item');"
+    # ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('order_line', 'ol_w_id');"
+    # ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "SELECT create_distributed_table('stock', 's_w_id');"
+
+    # ALTER TABLE order_line ADD FOREIGN KEY (OL_I_ID) REFERENCES item(I_ID);
+    # ALTER TABLE stock ADD FOREIGN KEY (S_I_ID) REFERENCES item(I_ID);
 
     ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "\copy WAREHOUSE from "${DATADIR}/warehouse.csv" with csv"
     ${INSTALLDIR}/bin/psql -U cs4224a -d $PGDATABASE -c "\copy DISTRICT from "${DATADIR}/district.csv" with csv"
