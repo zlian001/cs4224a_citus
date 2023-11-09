9edf50e48749246cc7de4988db4a0b1072af69e6
zlian001 <e0949137@u.nus.edu>
2023-11-09 17:08:30 +0800

    sdfsdf

--- new.sh
+++ new.sh
@@ -8,5 +8,5 @@
 #SBATCH --mem-per-cpu=2G   # memory per CPU core
 #SBATCH --cpus-per-task=4 # CPUs per srun task
 
-srun --nodes=5 --ntasks=5 --cpus-per-task=4 --nodelist=xcnd[20-24] ${INSTALLDIR}/bin/pg_ctl -D ${TEMPDIR} -l ${LOGFILE} -o "-p ${PGPORT}" start
+srun --nodes=1 --ntasks=1 --cpus-per-task=4 --nodelist=xcnd20 ${INSTALLDIR}/bin/pg_ctl -D ${TEMPDIR} -l ${LOGFILE} -o "-p ${PGPORT}" start
 sleep 3600
