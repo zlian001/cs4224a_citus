fd422d8f16f69b6e1ac63394a30a2d2ae9d21dc4
zlian001 <e0949137@u.nus.edu>
2023-11-09 18:48:51 +0800

    wdwqd

--- transactions.py
+++ transactions.py
@@ -56,7 +56,7 @@ class Transactions:
                                 ["getItemInfo"], (curr_s_i_id,))
                     I = cur.fetchone()
                     cur.execute(self.stmts['NEW_ORDER']["getStockInfo"],
-                                (d_id, curr_s_i_id, curr_s_wh_id))
+                                (d_id:02d, curr_s_i_id, curr_s_wh_id))
                     S = cur.fetchone()
                     adj_qty = S[0] - curr_deci_qty
 
