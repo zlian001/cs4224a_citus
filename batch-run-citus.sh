b008a5b03a4fe028b95c05400fcbeac67b9e0c63
Rohith Reddy Sankuru <e0963010@u.nus.edu>
2023-11-10 17:59:02 +0800

    batch ver_13

--- batch-run-citus.sh
+++ batch-run-citus.sh
@@ -6,7 +6,7 @@
 #SBATCH --error=/home/stuproj/cs4224a/cs4224a_citus/slurm_output/citus_batch-%j.err
 #SBATCH --nodelist=xcnd[45-49]
 #SBATCH --mem-per-cpu=2G   # memory per CPU core
-#SBATCH --cpus-per-task=4 # CPUs per srun task
+#SBATCH --cpus-per-task=24 # CPUs per srun task
 
 # proj variables
 INSTALLDIR=$HOME/pgsql
@@ -21,6 +21,9 @@ SCRIPTSDIR="$HOME/project_files/scripts"
 COORD="xcnd45"
 WORKERS="xcnd46;xcnd47;xcnd48;xcnd49"
 CLUSTER_IPS="xcnd45,xcnd46,xcnd47,xcnd48,xcnd49"
+CLUSTER_NODES="xcnd[45-49]"
+PREFIX="xcnd"
+START_NODE=45
 NODE=$(hostname)
 
 # define tasks flags with default values
@@ -46,7 +49,7 @@ done
 # deploy CITUS and project files
 if $deploy_citus; then
     echo $(logtime) "deploying CITUS cluster"
-    srun --nodes=5 --ntasks=5 --cpus-per-task=4 --nodelist=xcnd[45-49] ${SCRIPTSDIR}/deploy-citus.sh ${COORD} ${WORKERS[@]} &
+    srun --nodes=5 --ntasks=5 --cpus-per-task=16 --nodelist=${CLUSTER_NODES} ${SCRIPTSDIR}/deploy-citus.sh ${COORD} ${WORKERS[@]} &
     srun cp -rp $HOME/project_files /temp/cs4224a/
     echo $(logtime) "copied project data and xact files to nodes"
 fi
@@ -54,7 +57,8 @@ fi
 # start CITUS cluster
 if $start_citus; then
     echo $(logtime) "starting CITUS cluster"
-    srun --nodes=5 --ntasks=5 --cpus-per-task=4 --nodelist=xcnd[45-49] ${INSTALLDIR}/bin/pg_ctl -D ${TEMPDIR} -l ${LOGFILE} -o "-p ${PGPORT}" start &
+    #srun --nodes=5 --ntasks=5 --cpus-per-task=4 --nodelist=xcnd[45-49] ${INSTALLDIR}/bin/pg_ctl -D ${TEMPDIR} -l ${LOGFILE} -o "-p ${PGPORT}" start &
+    srun --nodes=5 --ntasks=5 --cpus-per-task=16 --nodelist=${CLUSTER_NODES} ${INSTALLDIR}/bin/postgres -D ${TEMPDIR} &
     sleep 60
     echo $(logtime) "node ${NODE}: $(ps -ef | grep postgres | grep -v grep)"
     if [ ${NODE} = "$COORD" ]; then
@@ -66,7 +70,7 @@ fi
 # creating schemas and loading data from COORD node
 if $load_data; then
     echo $(logtime) "creating table schemas and loading data using ${COORD}"
-    srun --nodes=5 --ntasks=5 --cpus-per-task=4 --nodelist=xcnd[45-49] ${SCRIPTSDIR}/load-citus-data.sh ${COORD} &
+    srun --nodes=5 --ntasks=5 --cpus-per-task=4 --nodelist=${CLUSTER_NODES} ${SCRIPTSDIR}/load-citus-data.sh ${COORD} &
 fi
 
 # execute transactions
@@ -85,17 +89,17 @@ if $exec_transactions; then
 
     pids=()
     for i in {0..19}; do
-        server=xcnd$((45 + $i % 5))
+        server=${PREFIX}$((${START_NODE} + $i % 5))
         echo $(logtime) "client${i} executing ${i}.txt from ${server}"
-        srun --nodes=1 --ntasks=1 --cpus-per-task=2 --nodelist=${server} python3 ${SCRIPTSDIR}/main-driver.py ${i} ${CLUSTER_IPS} < ${XACTDIR}/${i}.txt 1> ${RESULTSDIR}/client${i}_xacts.log 2> ${RESULTSDIR}/client${i}_xact_metrics.log &
+        srun --nodes=1 --ntasks=1 --cpus-per-task=2 --nodelist=${server} python3 ${SCRIPTSDIR}/main-driver.py ${i} ${CLUSTER_IPS} ${RESULTSDIR} < ${XACTDIR}/${i}.txt 1> ${RESULTSDIR}/client${i}_xacts.log 2> ${RESULTSDIR}/client${i}_xact_metrics.log &
         pids+=($!)
     done
-    
+
     # print out PIDs
     for pid in ${pids[@]}; do
         echo $(logtime) "PID: $pid"
     done
-    
+
     # wait for all transaction tasks to finish
     for pid in ${pids[*]}; do
         wait $pid
@@ -103,11 +107,11 @@ if $exec_transactions; then
 
     # generate performance metrics from xcnd45
     #echo $(logtime) "writing performance metrics to csv files"
-    #srun --nodes=1 --ntasks=1 --cpus-per-task=2 --nodelist=xcnd45 python3 ${SCRIPTSDIR}/generate_metrics.py ${CLUSTER_IPS} ${RESULTSDIR}
+    #srun --nodes=1 --ntasks=1 --cpus-per-task=2 --nodelist=${COORD} python3 ${SCRIPTSDIR}/generate_metrics.py ${CLUSTER_IPS} ${RESULTSDIR}
 fi
 
 # gracefully kill CITUS after Xact experiment tasks exits
 echo $(logtime) "stopping all CITUS nodes"
-#srun --nodes=5 --ntasks=5 --cpus-per-task=4 --nodelist=xcnd[45-49] ${INSTALLDIR}/bin/pg_ctl stop
-sleep 3600
+#srun --nodes=5 --ntasks=5 --cpus-per-task=2 --nodelist=${CLUSTER_NODES} ${INSTALLDIR}/bin/pg_ctl stop
+#rm -rf ${TEMPDIR}
 wait
